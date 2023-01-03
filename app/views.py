9d81e667b82abb28a21b3a20d94fb1b71eb40d35
Marcin-Fraszczak <kolothomasa@wp.pl>
2023-01-03 21:01:16 +0100

    pagination almost done

--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
+import json
+
+from django.core.serializers import serialize
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
@@ -9,11 +12,29 @@ class HomeView(View):
         all_donations = models.Donation.objects.all()
         total_bags = sum([int(donation.quantity) for donation in all_donations])
         total_institutions = len(set([donation.institution for donation in all_donations]))
-        context = {"total_bags": total_bags, "total_institutions": total_institutions}
 
-        if "fetch" in request.GET:
+        context = {
+            "total_bags": total_bags,
+            "total_institutions": total_institutions,
+        }
+        if "fetch_stats" in request.GET:
             data = context
             return JsonResponse(data)
+
+        foundations = models.Institution.objects.filter(type=1)
+        organizations = models.Institution.objects.filter(type=2)
+        collections = models.Institution.objects.filter(type=3)
+        results_per_page = 5
+
+        context.update({
+            "foundations": foundations,
+            "foundations_pages": [i for i in range(1, -(-len(foundations) // results_per_page) + 1)],
+            "organizations": organizations,
+            "organizations_pages": [i for i in range(1, -(-len(organizations) // results_per_page) + 1)],
+            "collections": collections,
+            "collections_pages": [i for i in range(1, -(-len(collections) // results_per_page) + 1)],
+            "results_per_page": results_per_page,
+        })
 
         return render(request, 'index.html', context)
 
