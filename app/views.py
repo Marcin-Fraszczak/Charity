fb54b8620571e11c74d9350f5baad86b20441024
Marcin-Fraszczak <kolothomasa@wp.pl>
2023-01-03 15:22:59 +0100

    added bag and institution counter with fetch

--- app/views.py
+++ app/views.py
@@ -1,10 +1,21 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
+from . import models
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'index.html')
+        all_donations = models.Donation.objects.all()
+        total_bags = sum([int(donation.quantity) for donation in all_donations])
+        total_institutions = len(set([donation.institution for donation in all_donations]))
+        context = {"total_bags": total_bags, "total_institutions": total_institutions}
+
+        if "fetch" in request.GET:
+            data = context
+            return JsonResponse(data)
+
+        return render(request, 'index.html', context)
 
 
 class AddDonationView(View):
