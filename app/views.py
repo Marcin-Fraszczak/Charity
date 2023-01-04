82665e084a37f449571f1fb204ac682bb44b3e37
Marcin-Fraszczak <kolothomasa@wp.pl>
2023-01-04 10:16:19 +0100

    pagination done well

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 import json
 
+from django.core.paginator import Paginator
 from django.core.serializers import serialize
 from django.http import JsonResponse
 from django.shortcuts import render
@@ -21,19 +22,27 @@ class HomeView(View):
             data = context
             return JsonResponse(data)
 
-        foundations = models.Institution.objects.filter(type=1)
-        organizations = models.Institution.objects.filter(type=2)
-        collections = models.Institution.objects.filter(type=3)
+        foundations = models.Institution.objects.filter(type=1).order_by("name")
+        organizations = models.Institution.objects.filter(type=2).order_by("name")
+        collections = models.Institution.objects.filter(type=3).order_by("name")
         results_per_page = 5
 
+        paginated_foundations = Paginator(foundations, results_per_page)
+        pf_dict = {page: paginated_foundations.page(page) for page in paginated_foundations.page_range}
+        pf_page_range = paginated_foundations.get_elided_page_range(
+            paginated_foundations.num_pages, on_each_side=2, on_ends=2)
+
+        paginated_organizations = Paginator(organizations, results_per_page)
+        po_dict = {page: paginated_organizations.page(page) for page in paginated_organizations.page_range}
+        paginated_collections = Paginator(collections, results_per_page)
+        pc_dict = {page: paginated_collections.page(page) for page in paginated_collections.page_range}
+
+
         context.update({
-            "foundations": foundations,
-            "foundations_pages": [i for i in range(1, -(-len(foundations) // results_per_page) + 1)],
-            "organizations": organizations,
-            "organizations_pages": [i for i in range(1, -(-len(organizations) // results_per_page) + 1)],
-            "collections": collections,
-            "collections_pages": [i for i in range(1, -(-len(collections) // results_per_page) + 1)],
-            "results_per_page": results_per_page,
+            "pf_dict": pf_dict,
+            "pf_page_range": pf_page_range,
+            "po_dict": po_dict,
+            "pc_dict": pc_dict,
         })
 
         return render(request, 'index.html', context)
