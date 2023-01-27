a8111e0253930b6615e774d36077714274fa1567
Marcin-Fraszczak <kolothomasa@wp.pl>
2023-01-27 15:59:34 +0100

    config for localhost on any machine

--- config/settings.py
+++ config/settings.py
@@ -29,10 +29,13 @@ environ.Env.read_env(os.path.join(BASE_DIR, 'env', '.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJ_SECRET_KEY')
+# SECRET_KEY = os.environ.get('DJ_SECRET_KEY')
+SECRET_KEY = '4fsw5ey0@!&s9ff=$2cv^ztqb+0y4h-l==^!^ebrl3mmq^#7$1'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get('DJ_DEBUG', default=0))
+# DEBUG = int(os.environ.get('DJ_DEBUG', default=0))
+DEBUG = 1
 
 ALLOWED_HOSTS = ['eurobyvelo.eu.pythonanywhere.com', '127.0.0.1', 'localhost']
 
@@ -86,7 +89,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# mySQL for pythonanywhere
+"""
+1. MySQL setup for pythonanywhere
+"""
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
@@ -97,25 +102,31 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     }
 # }
 
-# Postgres setup
-DATABASES = {
-    'default': {}
-}
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
-db_from_env = dj_database_url.config(
-    default=DATABASE_URL, conn_max_age=600, ssl_require=False
-)
-
-DATABASES['default'].update(db_from_env)
-
-# Old one sqlite
+"""
+2. Postgres setup for localhost
+"""
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+#     'default': {}
 # }
+#
+# DATABASE_URL = os.environ.get('DATABASE_URL')
+# db_from_env = dj_database_url.config(
+#     default=DATABASE_URL, conn_max_age=600, ssl_require=False
+# )
+#
+# DATABASES['default'].update(db_from_env)
+
+
+"""
+3. SQLite setup for localhost
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
