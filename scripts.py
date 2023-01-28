9504741ce29d93ea84f7ed599a4218805d62ff97
Marcin-Fraszczak <kolothomasa@wp.pl>
2023-01-28 14:02:37 +0100

    python script populating database

--- scripts.py
+++ scripts.py
@@ -1,24 +1,24 @@
-import subprocess
+import os
+from random import choices, randint
 
+import django
 from django.contrib.auth import get_user_model
-from django.core.management import call_command
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
 
 from app import models
 
 User = get_user_model()
-counter = {
-    "admin": 1,
-    "user": 1,
-}
 
 
-def check_if_taken(name, admin=False):
-    existing_users = User.objects.filter(email=f'{name}{counter[name]}@gmail.com')
+def check_if_taken(name, counter, admin=False):
+    email = f'{name}{counter}@gmail.com'
+    existing_users = User.objects.filter(email=email)
     if existing_users:
-        counter[name] += 1
-        check_if_taken(name, admin)
-    else:
-        return f'{name}{counter[name]}@gmail.com'
+        counter += 1
+        email = check_if_taken(name, counter, admin)
+    return email
 
 
 def create_user(admin=False):
@@ -33,32 +33,78 @@ def create_user(admin=False):
     else:
         name = "user"
 
-    email = check_if_taken(name, admin)
+    email = check_if_taken(name, 1, admin)
     password = "Testpass123"
 
-    user = User(email=email)
+    user = User(email=email, username=email)
     user.set_password(password)
     user.is_active = 1
     user.is_staff = admin
     user.is_superuser = admin
     user.save()
 
-    return user
+    return user, password
 
 
-def start():
-    subprocess.run("pip install -r requirements.txt")
-    call_command("migrate", interactive=False)
-    user = create_user(admin=False)
-    superuser = create_user(admin=True)
+def create_categories():
+    categories = ["zabawki", "AGD", "żywność", "ubrania", "sprzęt sportowy", "lekarstwa", "złom", "broń", "amunicja"]
+    for cat in categories:
+        models.Category.objects.create(name=cat)
+        print(cat)
+
+
+def create_institutions():
+    categories = [cat for cat in models.Category.objects.all()]
+    institutions = [
+        ("Biedni Szwajcarzy", "Wymiarowy basen w domu każdego Szwjcara", 1),
+        ("Kasztanowy domek", "Świetlice środowiskowe dla dzieci", 1),
+        ("Animals", "Opieka nad porzuconymi zwierzakami", 1),
+        ("Życie z wystawki", "Byle co, byle za darmo", 1),
+        ("Dzieciaki na zewnątrz", "Krzewienie kultury fizycznej i aktywności na świeżym powietrzu wśród młodzieży", 1),
+        ("Dżihad", "Szybka droga do raju", 2),
+        ("Caritas", "Pomoc potrzebującym w kraju i za granicą", 2),
+        ("WOŚP", "Wielka Orkiestra", 2),
+        ("Heniu", "Szuka złomu i małego AGD", 3),
+        ("Roman", "Szuka części do Passata", 3),
+        ("Stefan", "Zbiera na piwo", 3),
+        ("Zyta", "Chce czuć się bezpiecznie na Wildzie", 3),
+    ]
+    for inst in institutions:
+        institution = models.Institution(
+            name=inst[0],
+            description=inst[1],
+            type=inst[2],
+        )
+        institution.save()
+        random_cats = choices(categories, k=randint(1, 4))
+        institution.categories.set(random_cats)
+        institution.save()
+        print(inst)
+
+
+def populate():
     print(f'=' * 60)
+    user, password = create_user(admin=False)
     print(f'User created:')
     print(f'email: \t {user.email}')
-    print(f'password: {user.password}')
+    print(f'password: {password}')
     print(f'=' * 60)
+    superuser, password = create_user(admin=True)
     print(f'Superuser created:')
     print(f'email: \t {superuser.email}')
-    print(f'password: {superuser.password}')
+    print(f'password: {password}')
     print(f'=' * 60)
-    call_command("runserver", interactive=False)
-    print("Go check your default web browser")
+    print("Created categories:")
+    create_categories()
+    print(f'=' * 60)
+    print("Created institutions:")
+    create_institutions()
+    print(f'=' * 60)
+
+
+def say_hello():
+    print("hello")
+
+
+if __name__ == '__main__':
+    populate()
